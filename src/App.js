91f582c6176472f77cd8622f3b0486abfac5a349
Fawaz Al-Mutairi <q8fawazo@hotmail.com>
2020-09-29 15:14:09 +0300

    Fawaz Changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Data
 import authors from "./data";
@@ -6,16 +6,43 @@ import authors from "./data";
 // Components
 import Sidebar from "./Sidebar";
 import AuthorList from "./AuthorList";
+import AuthorDetail from "./AuthorDetail";
 
 function App() {
+  const [currentAuthor, setAuthor] = useState(null);
+  const [Authors, setAuthors] = useState(authors);
+
+  const selectAuthor = author => {
+    setAuthor(author);
+    console.log(author);
+  };
+  const resetAuthor = () => {
+    setAuthor(null);
+  };
+  const filterAuthors = query => {
+    let newAuthors = authors.filter(
+      author =>
+        author.first_name.includes(query) || author.last_name.includes(query)
+    );
+    setAuthors(newAuthors);
+  };
+
   return (
     <div id="app" className="container-fluid">
       <div className="row">
         <div className="col-2">
-          <Sidebar />
+          <Sidebar resetAuthor={resetAuthor} />
         </div>
         <div className="content col-10">
-          <AuthorList authors={authors} />
+          {currentAuthor ? (
+            <AuthorDetail author={currentAuthor} />
+          ) : (
+            <AuthorList
+              authors={Authors}
+              selectAuthor={selectAuthor}
+              filterAuthors={filterAuthors}
+            />
+          )}
         </div>
       </div>
     </div>
